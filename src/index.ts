71d3741c78bbaf3a97b686a3d7d01123ae1967a3
Shawn Smith <sds.smith24@gmail.com>
2026-02-23 16:23:46 -0500

    prune component selection to focus on liquid glass components

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,15 @@ export { default as ThemeProvider, useTheme } from './theme/ThemeProvider';
 export type { LiquidGlassTheme, ThemeTokenKey } from './theme/types';
 
 /* --- Atoms --- */
-export { default as Alert } from './ui/atoms/Alert';
 export { default as Avatar } from './ui/atoms/Avatar';
 export { default as Button } from './ui/atoms/Button';
 export { default as Chip } from './ui/atoms/Chip';
-export { CollapsibleRoot, CollapsibleContent } from './ui/atoms/Collapsible';
 export { default as Fab } from './ui/atoms/Fab';
 export { default as IconButton } from './ui/atoms/IconButton';
 export { default as Separator } from './ui/atoms/Separator';
 export { default as Skeleton } from './ui/atoms/Skeleton';
 export { default as Spinner } from './ui/atoms/Spinner';
 
-/* --- Icons --- */
-export { default as AddIcon } from './ui/atoms/icons/AddIcon';
-export { default as CameraAltIcon } from './ui/atoms/icons/CameraAltIcon';
-export { default as CloudUploadIcon } from './ui/atoms/icons/CloudUploadIcon';
-export { default as DeleteIcon } from './ui/atoms/icons/DeleteIcon';
-export { default as MoreVertIcon } from './ui/atoms/icons/MoreVertIcon';
-export { default as PhotoLibraryIcon } from './ui/atoms/icons/PhotoLibraryIcon';
 
 /* --- Molecules --- */
 export {
@@ -37,7 +28,6 @@ export {
   AlertDialogAction,
 } from './ui/molecules/AlertDialog';
 export { default as Card, CardContent, CardMedia } from './ui/molecules/Card';
-export { default as CommentInput } from './ui/molecules/CommentInput';
 export {
   DialogRoot,
   DialogTrigger,
@@ -54,13 +44,10 @@ export {
   DropdownMenuItem,
 } from './ui/molecules/DropdownMenu';
 export { default as Select } from './ui/molecules/Select';
-export { TabsRoot, TabsList, TabsTrigger, TabsContent } from './ui/molecules/Tabs';
 export { default as TextField } from './ui/molecules/TextField';
 
 /* --- Organisms --- */
 export { default as AppBar } from './ui/organisms/AppBar';
 
 /* --- Layout --- */
-export { default as Container } from './ui/layout/Container';
 export { default as Paper } from './ui/layout/Paper';
-export { default as Stack } from './ui/layout/Stack';
