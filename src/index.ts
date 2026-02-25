9a5bef533730e89a41d582751423c48b1cd924a0
Shawn Smith <sds.smith24@gmail.com>
2026-02-25 07:20:05 -0500

    move components into component folders

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,21 @@ export * from "./theme/ThemeProvider";
 export type { LiquidGlassTheme, ThemeTokenKey } from "./theme/types";
 
 /* --- Inputs --- */
-export { default as Button } from "./ui/inputs/Button";
-export { default as Fab } from "./ui/inputs/Fab";
-export { default as IconButton } from "./ui/inputs/IconButton";
-export { default as Select } from "./ui/inputs/Select";
-export { default as TextField } from "./ui/inputs/TextField";
+export { default as Button } from "./ui/inputs/Button/Button";
+export { default as Fab } from "./ui/inputs/Fab/Fab";
+export { default as IconButton } from "./ui/inputs/IconButton/IconButton";
+export { default as Select } from "./ui/inputs/Select/Select";
+export { default as TextField } from "./ui/inputs/TextField/TextField";
 
 /* --- Surfaces --- */
-export { default as AppBar } from "./ui/surfaces/AppBar";
-export { default as Card, CardContent, CardMedia } from "./ui/surfaces/Card";
-export { default as Paper } from "./ui/surfaces/Paper";
+export { default as AppBar } from "./ui/surfaces/AppBar/AppBar";
+export { default as Card, CardContent, CardMedia } from "./ui/surfaces/Card/Card";
+export { default as Paper } from "./ui/surfaces/Paper/Paper";
 
 /* --- Data Display --- */
-export { default as Avatar } from "./ui/data-display/Avatar";
-export { default as Chip } from "./ui/data-display/Chip";
-export { default as Separator } from "./ui/data-display/Separator";
+export { default as Avatar } from "./ui/data-display/Avatar/Avatar";
+export { default as Chip } from "./ui/data-display/Chip/Chip";
+export { default as Separator } from "./ui/data-display/Separator/Separator";
 
 /* --- Feedback --- */
 export {
@@ -32,7 +32,7 @@ export {
   AlertDialogActions,
   AlertDialogCancel,
   AlertDialogAction,
-} from "./ui/feedback/AlertDialog";
+} from "./ui/feedback/AlertDialog/AlertDialog";
 export {
   DialogRoot,
   DialogTrigger,
@@ -41,9 +41,9 @@ export {
   DialogDescription,
   DialogActions,
   DialogClose,
-} from "./ui/feedback/Dialog";
-export { default as Skeleton } from "./ui/feedback/Skeleton";
-export { default as Spinner } from "./ui/feedback/Spinner";
+} from "./ui/feedback/Dialog/Dialog";
+export { default as Skeleton } from "./ui/feedback/Skeleton/Skeleton";
+export { default as Spinner } from "./ui/feedback/Spinner/Spinner";
 
 /* --- Navigation --- */
 export {
@@ -51,4 +51,4 @@ export {
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
-} from "./ui/navigation/DropdownMenu";
+} from "./ui/navigation/DropdownMenu/DropdownMenu";
